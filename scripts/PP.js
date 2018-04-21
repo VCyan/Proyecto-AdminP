5256171bf766ac8dc71d8a43311878734855846a
VCyan10 <martinvictor45@hotmail.com>
2018-04-21 16:41:02 -0500

    Avanzando en el proyecto PP.
    Signed-off-by: VCyan10 <martinvictor45@hotmail.com>

--- scripts/PP.js
+++ scripts/PP.js
@@ -2,26 +2,38 @@
 		var vPeriod = document.getElementById("vPeriod");
 		var vPeriodValue = vPeriod.options[vPeriod.selectedIndex].value;
 		
-		//document.getElementById("divPP").innerHTML = "Paragraph changed!"+vPeriodValue;
-		//http://garystorey.com/2017/02/27/three-ways-to-create-dom-elements-without-jquery/
-		var eTable = document.createElement('tablePP');
-		var domString = '<table class="table table-bordered table-hover" id="tablePP">';
-		domString = domString + '<thead class="thead-dark"><tr><th scope="col"># Period</th><th scope="col">Infows</th><th scope="col">Outflows</th><th scope="col">Cummulative Cashflow</th></tr></thead>';
-		
-		domString = domString + '<tbody>';
-		
-		for(i=1; i <= vPeriodValue; i++)
-		{
-			domString = domString + '<tr><th scope="row">'+ i +'</th>';
-				domString = domString + '<td><input type="number" class="form-control" id="inflowPP'+i+'"></td>';
-				domString = domString + '<td><input type="number" class="form-control" id="outflowPP'+i+'"></td>';
-				domString = domString + '<td><input class="form-control" id="cummCfPP'+i+'" type="text" placeholder="Disabled input here..." disabled></td>';
-			domString = domString + '</tr>';
+		if(vPeriodValue == 0){
+			var domString = '<table class="table table-bordered table-hover" id="tablePP">';
+			domString = domString + '<thead class="thead-dark"><tr><th scope="col"># Period</th><th scope="col">Infows</th><th scope="col">Outflows</th><th scope="col">Cummulative Cashflow</th></tr></thead>';
+			
+			domString = domString + '<tbody>';
+		}else{
+			//document.getElementById("divPP").innerHTML = "Paragraph changed!"+vPeriodValue;
+			//http://garystorey.com/2017/02/27/three-ways-to-create-dom-elements-without-jquery/
+			var eTable = document.createElement('tablePP');
+			var domString = '<table class="table table-bordered table-hover" id="tablePP">';
+			domString = domString + '<thead class="thead-dark"><tr><th scope="col"># Period</th><th scope="col">Infows</th><th scope="col">Outflows</th><th scope="col">Cummulative Cashflow</th></tr></thead>';
+			
+			domString = domString + '<tbody>';
+			
+			for(i=1; i <= vPeriodValue; i++)
+			{
+				domString = domString + '<tr><th scope="row">'+ i +'</th>';
+					domString = domString + '<td><input type="number" class="form-control" id="inflowPP'+i+'"></td>';
+					domString = domString + '<td><input type="number" class="form-control" id="outflowPP'+i+'"></td>';
+					domString = domString + '<td><input class="form-control" id="cummCfPP'+i+'" type="text" placeholder="Disabled input here..." disabled></td>';
+				domString = domString + '</tr>';
+			}
 		}
 		
 		domString = domString + '</tbody></table>';
 		
 		document.getElementById("divPP").innerHTML = domString;
+	}
+	
+	function setSelectValue() {
+		var element = document.getElementById('vPeriod');
+		element.value = 3;
 	}
 	
 	function generate_TableNPV() {
