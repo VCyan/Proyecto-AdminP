d1ef67cc98a7ce40b737174e47639f121b339b3f
VCyan10 <martinvictor45@hotmail.com>
2018-04-23 09:27:50 -0500

    Adding Project Scerening Method.
    Signed-off-by: VCyan10 <martinvictor45@hotmail.com>

--- scripts/PP.js
+++ scripts/PP.js
@@ -3,11 +3,11 @@
 		//alert(vPeriodValue);
 		//var vPeriod = document.getElementById("vPeriod");
 		//var vPeriodValue = vPeriod.options[vPeriod.selectedIndex].value;
-		
+
 		if(vPeriodValue == 0 || null){
 			var domString = '<table class="table table-bordered table-hover" id="tablePP">';
 			domString = domString + '<thead class="thead-dark"><tr><th scope="col"># Period</th><th scope="col">Infows</th><th scope="col">Outflows</th><th scope="col">Cummulative Cashflow</th></tr></thead>';
-			
+
 			domString = domString + '<tbody>';
 		}else{
 			//document.getElementById("divPP").innerHTML = "Paragraph changed!"+vPeriodValue;
@@ -15,9 +15,9 @@
 			var eTable = document.createElement('tablePP');
 			var domString = '<table class="table table-bordered table-hover" id="tablePP">';
 			domString = domString + '<thead class="thead-dark"><tr><th scope="col"># Period</th><th scope="col">Infows</th><th scope="col">Outflows</th><th scope="col">Cummulative Cashflow</th></tr></thead>';
-			
+
 			domString = domString + '<tbody>';
-			
+
 			for(i=1; i <= vPeriodValue; i++)
 			{
 				domString = domString + '<tr><th scope="row">'+ i +'</th>';
@@ -27,12 +27,12 @@
 				domString = domString + '</tr>';
 			}
 		}
-		
+
 		domString = domString + '</tbody></table>';
-		
+
 		document.getElementById("divPP").innerHTML = domString;
 	}
-	
+
 	function validateData() {
 		var vPeriod 	= document.getElementById("vPeriod").value;
 		var vPrincipal 	= document.getElementById("vPrincipal").value;
@@ -42,7 +42,7 @@
 		// alert("vPrincipal " + vPrincipal);
 		// alert("vTasa " + vTasa);
 		var domString = '<label for="colFormLabelLg" class="col-sm-10 col-form-label col-form-label-lg">';
-		
+
 		if(isNaN(vPeriod) || vPeriod=="" || vPeriod==null || vPeriod==0){
 			domString = domString + "ERROR: El número de PERIODOS no está definido...<br>";
 		}
@@ -59,14 +59,14 @@
 		else if(vTasa < 0 || vTasa > 100){
 			domString = domString + "ERROR: La TASA DE INTERÉS no debe ser menor a 0 o mayor a 100...<br>";
 		}
-		
+
 		domString = domString + '</label>';
 		document.getElementById("errorPP").innerHTML = domString;
-		
+
 		if(!isNaN(vPeriod) && vPeriod!="" && vPeriod!=null && !isNaN(vPrincipal) && vPrincipal!="" && vPrincipal!=null && !isNaN(vTasa) && vTasa!="" && vTasa!=null && vTasa <= 100 && vTasa >= 0){
 			// Call php: PHP and AJAX Example: https://www.tutorialspoint.com/php/php_and_ajax.htm
 			var ajaxRequest;  // The variable that makes Ajax possible!
-			   
+
 			try {
 			  // Opera 8.0+, Firefox, Safari
 			  ajaxRequest = new XMLHttpRequest();
@@ -76,7 +76,7 @@
 				 ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
 			  }catch (e) {
 				 // code for IE6, IE5
-				 try{				
+				 try{
 					ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
 				 }catch (e){
 					// Something went wrong
@@ -85,10 +85,10 @@
 				 }
 			  }
 			}
-			// Create a function that will receive data 
+			// Create a function that will receive data
 			// sent from the server and will update
 			// div section in the same page.
-				
+
 			ajaxRequest.onreadystatechange = function(){
 			  if(ajaxRequest.readyState == 4){
 				 var ajaxDisplay = document.getElementById('divPP');
@@ -98,7 +98,7 @@
 
 			// Now get the value from user and pass it to
 			// server script.
-			
+
 			// http://www.kvcodes.com/2015/10/passing-javascript-array-to-php-through-jquery-ajax
 			// Get values from Inflows & Outflows into JQuery Arrays encoded as JSON:
 			var inflowsPP  = [];
@@ -112,40 +112,40 @@
 			//var myJSONText = JSON.stringify( arrayfromjs );
 			var JSONinflowsPP 	= JSON.stringify( inflowsPP );
 			var JSONoutflowsPP 	= JSON.stringify( outflowsPP );
-			
+
 			// var age = document.getElementById('age').value;
 			// var wpm = document.getElementById('wpm').value;
 			// var sex = document.getElementById('sex').value;
-			
+
 			var queryString = "?vPeriod=" + vPeriod +"&vPrincipal=" + vPrincipal + "&vTasa=" + vTasa + "&inflowsPP="+JSONinflowsPP + "&outflowsPP="+JSONoutflowsPP;
 			ajaxRequest.open("GET", "PP.php" + queryString, true);
 			ajaxRequest.send(null);
-			   
+
 			//document.getElementById("errorPP").innerHTML = "DAsd";
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 	function generate_TableNPV() {
 		var vPeriod = document.getElementById("vPeriod2");
 		var vPeriodValue = vPeriod2.options[vPeriod2.selectedIndex].value;
-		
+
 		//document.getElementById("divPP").innerHTML = "Paragraph changed!"vPeriodValue;
-		
+
 		var eTable = document.createElement('tableNPV');
 		var domString = '<table class="table table-hover table-dark" id="tableNPV">';
 		domString = domString + '<thead class="thead-dark"><tr><th scope="col"># Period</th><th scope="col">Inflows</th><th scope="col">Outflows</th><th scope="col">Cummulative Cashflow</th></tr></thead>';
-		
+
 		domString = domString + '<tbody>';
-		
+
 		for(i=0; i <= vPeriodValue; i++)
 		{
 			domString = domString + '<tr><th scope="row">'+ i +'</th>';
@@ -158,8 +158,8 @@
 			domString = domString + '<td colspan="3"> Net Present Value </td>';
 			domString = domString + '<td><form><input type="text"></form></td>';
 			domString = domString + '</tr>';
-		
+
 		domString = domString + '</tbody></table>';
-		
+
 		document.getElementById("divNPV").innerHTML = domString;
 	}
